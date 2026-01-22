d2dde12019223586518f14394dfe96f75a7dee20
ichwalM <13020230049@student.umi.ac.id>
2026-01-23 05:37:46 +0800

    remove route regist and forgot pass

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
