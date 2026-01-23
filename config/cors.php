230738765ee4391a3301ec4a0f3d384c3d6b5fc4
ichwalM <13020230049@student.umi.ac.id>
2026-01-24 04:02:32 +0800

    for deploy

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:8000', 'http://localhost:3000', 'site.walldev.my.id'],
+    'allowed_origins' => ['http://localhost:5173', 'http://localhost:8000', 'http://localhost:3000', 'app.walldev.my.id'],
 
     'allowed_origins_patterns' => [],
 
